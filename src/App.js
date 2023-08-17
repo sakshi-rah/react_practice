235fd3c9267017e34d7d6ebc1e3e46e2004f9cd5
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-08-17 11:37:14 +0530

    feat: RandomNo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import UseRefHook from './UseRefHook';
 import FormValidation from './FormValidation'
+import RandomNo from './RandomNo';
 
 function App() {
   return (
-
     <div className="App">
+
+    <RandomNo/>
+
       <BrowserRouter>
         <Routes>
           <Route path='/useRefHook' element={<UseRefHook />} />
